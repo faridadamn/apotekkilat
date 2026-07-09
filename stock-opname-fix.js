0bbd94ebf67226c0c23af88fe610986be3d14257
faridadamn <160025733+faridadamn@users.noreply.github.com>
2026-07-09 11:28:25 +0700

    Remove redundant stock opname render patch

--- stock-opname-fix.js
+++
@@ -1,5 +0,0 @@
-/* Hindari handler ganda tombol Mulai Stock Opname */
-(function(){
-  const baseRender=render;
-  render=function(){baseRender();document.querySelectorAll('[data-so-start]').forEach(b=>b.onclick=null);};
-})();
